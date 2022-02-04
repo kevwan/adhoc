c3ae8861640ae72bc0b54fc61d55b58b904d1467
kevin <wanjunfeng@gmail.com>
2022-02-04 22:44:22 +0800

    try win test

--- cli/cli.go
+++ cli/cli.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
 	"syscall"
 
 	"github.com/kevwan/adhoc"
 )
 
+func cancelOnSignals() {
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGINT, syscall.SIGTSTP, syscall.SIGQUIT)
+		select {
+		case <-c:
+			fmt.Println(`migrate failed, reason: "User Canceled"`)
+			os.Exit(0)
+		}
+	}()
+}
+
 func main() {
+	cancelOnSignals()
 	adhoc.SayHello("0.0.7-1")
-	_ = syscall.SIGTERM
 }
